0f7d9550a6922326e9d6d037d7886869a0dd958e
Trick22618 <pofgh986521@yahoo.com.tw>
2025-08-14 21:23:52 +0800

    update

---
+++ src/main/java/websocket/ChatServer.java
@@ -0,0 +1,66 @@
+package websocket;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/chatserver")
+public class ChatServer {
+	// 建立一個 List<session> 存放所有連線資訊
+	private static List<Session> sessions = new CopyOnWriteArrayList<Session>();
+	
+	// 廣播
+	private void broadcast(String sessionId, String message) {
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+		message = String.format("%8s %s 說 %s", sdf.format(new Date()), sessionId, message);
+		for(Session session : sessions) {
+			if(session.isOpen()) {
+				session.getAsyncRemote().sendText(message); // 將訊息回傳給 client 端
+				
+			}
+		}
+	}
+	
+	@OnOpen
+	public void onOpen(Session session) {
+		sessions.add(session);
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 已連線%n", sessionId);
+		String message = String.format("%s 已進入聊天室%n", sessionId);
+		broadcast(sessionId, message);
+	}
+	
+	@OnMessage
+	public void onMessage(Session session, String message) {
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 說: %s%n", sessionId, message);
+		message = String.format("%s 說: %s%n", sessionId, message);
+		broadcast(sessionId, message);
+	}
+	
+	@OnClose
+	public void onClose(Session session) {
+		sessions.remove(session);
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 已離線%n", sessionId);
+		String message = String.format("%s 已離開聊天室%n", sessionId);
+		broadcast(sessionId, message);
+	}
+	
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		String sessionId = session.getId();
+		System.out.printf("session id: %s 發生錯誤: %s%n", sessionId, throwable);
+	}
+
+}
